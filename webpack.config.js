516b0511a8f96601d30982d34e4e39e65cbbf585
Mateusz Widerski <mat.widerski@gmail.com>
2018-06-14 22:10:52 +0200

    bug fixes

--- webpack.config.js
+++ webpack.config.js
@@ -1,28 +1,31 @@
 const path = require('path');
+const webpack = require('webpack');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const OptimizeJsPlugin = require('optimize-js-plugin');
 
+let env = process.env.NODE_ENV || 'development';
 const plugins = [new HtmlWebpackPlugin({
 				template: 'src/index.html',
 				filename: 'index.html',
 				inject: 'body'
 			})];
 
+if (env === 'production') {
+	plugins.push(
+		new OptimizeJsPlugin({
+			sourceMap: false
+		})
+	)
+}
+
 //webpack.config.js
 module.exports = (env) => {
-	if (env === 'production') {
-		plugins.push(
-			new OptimizeJsPlugin({
-				sourceMap: false
-			})
-		)
-	}
 	return {
 		entry: (env !== 'production' ? [
-			'react-hol-loader/patch',
-			'webpack-dev-server/client?http://localhost:8080',
-			'webpack/hot/only-dev-server'
-		] : []).concat(['./client/index.js']),
+	        'react-hot-loader/patch',
+	        'webpack-dev-server/client?http://localhost:8080',
+	        'webpack/hot/only-dev-server',
+	    ] : []).concat(['./client/index.js']),
 		output: {
 			path: path.resolve(__dirname, 'public'),
 			filename: './bundle.js'
