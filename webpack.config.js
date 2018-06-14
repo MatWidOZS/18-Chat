0d4a934c1b6c4003fcb975e9a944cc7ea7bee045
Mateusz Widerski <mat.widerski@gmail.com>
2018-06-14 22:22:23 +0200

    Build app

--- webpack.config.js
+++ webpack.config.js
@@ -35,9 +35,6 @@ module.exports = (env) => {
 				{
 					test: /\.js$/,
 					loader: "babel-loader",
-					options: {
-						plugins: env !== 'production' ? ["react-hot-loader/babel"] : []
-					}
 				},
 				{
 					test: /\.css$/,
